db9ba4b4b13a9afe60b8725324431f940126b306
D'Arcy Rittich <drittich@gmail.com>
2021-11-13 12:14:52 -0500

    fix UI update bug

--- DnsTube2/TaskScheduler.cs
+++ DnsTube2/TaskScheduler.cs
@@ -1,40 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Threading;
+using System.Windows;
+using System.Windows.Threading;
 
 namespace DnsTube2
 {
-	public class TaskScheduler
-	{
-		static TaskScheduler _instance;
-		List<Timer> timers = new List<Timer>();
+    public class TaskScheduler
+    {
+        static TaskScheduler _instance;
+        List<Timer> timers = new List<Timer>();
 
-		TaskScheduler() { }
+        TaskScheduler() { }
 
-		public static TaskScheduler Instance => _instance ?? (_instance = new TaskScheduler());
+        public static TaskScheduler Instance => _instance ?? (_instance = new TaskScheduler());
 
-		public TaskScheduler ScheduleTask(TimeSpan interval, Action task, bool runImmediately = false)
-		{
-			var timeToFirstRun = runImmediately ? TimeSpan.Zero : interval;
+        public TaskScheduler ScheduleTask(TimeSpan interval, Action task, bool runImmediately = false)
+        {
+            var timeToFirstRun = runImmediately ? TimeSpan.Zero : interval;
 
-			var timer = new Timer(x =>
-			{
-				task.Invoke();
-			}, null, timeToFirstRun, interval);
-			timers.Add(timer);
+            var timer = new Timer(x =>
+            {
+                // Ref: https://stackoverflow.com/a/9732853/39430
+                Application.Current.Dispatcher.Invoke(task, DispatcherPriority.ContextIdle);
+            }, null, timeToFirstRun, interval);
+            timers.Add(timer);
 
-			return Instance;
-		}
+            return Instance;
+        }
 
-		public static void StopAll()
-		{
-			// Ref: https://stackoverflow.com/a/14359490/39430
-			if (_instance != null)
-			{
-				foreach (var timer in Instance.timers)
-					timer.Change(Timeout.Infinite, Timeout.Infinite);
-				_instance = null;
-			}
-		}
-	}
+        public static void StopAll()
+        {
+            // Ref: https://stackoverflow.com/a/14359490/39430
+            if (_instance != null)
+            {
+                foreach (var timer in Instance.timers)
+                    timer.Change(Timeout.Infinite, Timeout.Infinite);
+                _instance = null;
+            }
+        }
+    }
 }
