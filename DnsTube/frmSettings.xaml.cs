13e254f1c60158ac9d653db4fb2266c3d4bbe48c
D'Arcy Rittich <drittich@gmail.com>
2021-11-19 21:58:03 -0500

    add link to Cloudlfare docs

--- DnsTube/frmSettings.xaml.cs
+++ DnsTube/frmSettings.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,7 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace DnsTube
@@ -115,5 +117,13 @@ namespace DnsTube
             settings.IsUsingToken = rbUseApiToken.IsChecked.GetValueOrDefault();
             HandleAuthDisplay();
         }
-	}
+
+        private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            // for .NET Core you need to add UseShellExecute = true
+            // see https://docs.microsoft.com/dotnet/api/system.diagnostics.processstartinfo.useshellexecute#property-value
+            Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri) { UseShellExecute = true });
+            e.Handled = true;
+        }
+    }
 }
