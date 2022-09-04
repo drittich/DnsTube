3b7ebbd4d5109ff4a9a2ad22075965a5e92919e9
D'Arcy Rittich <drittich@gmail.com>
2022-09-04 08:52:41 -0400

    update publish script

--- publish.bat
+++ publish.bat
@@ -5,8 +5,6 @@ set PROJECT_PATH=%SOLUTION_PATH%\DnsTube
 set PUBLISH_PATH=%PROJECT_PATH%\bin\Publish
 set ZIP_EXE_PATH="C:\Program Files\7-Zip\7z.exe"
 
-echo Will create, e.g., DnsTube%EXE_VERSION%.exe
-
 if exist %PUBLISH_PATH%\NUL del /f/s/q %PUBLISH_PATH% > nul & rmdir /s/q %PUBLISH_PATH%
 
 cd %PROJECT_PATH%
@@ -18,8 +16,8 @@ set PORTABLE=PORTABLE
 :: portable 
 echo publishing portable version...
 dotnet publish --nologo -r win-x64 -c Release --self-contained false -o %PUBLISH_PATH%\TEMP
-ren %PUBLISH_PATH%\TEMP\DnsTube.exe DnsTube-Portable%EXE_VERSION%.exe
-ren %PUBLISH_PATH%\TEMP\DnsTube.pdb DnsTube-Portable%EXE_VERSION%.pdb
+ren %PUBLISH_PATH%\TEMP\DnsTube.exe DnsTube-Portable.exe
+ren %PUBLISH_PATH%\TEMP\DnsTube.pdb DnsTube-Portable.pdb
 
 cd %PUBLISH_PATH%\TEMP
 %ZIP_EXE_PATH% a -t7z -r -mx9 ..\DnsTube-Portable%EXE_VERSION%.7z *.*
@@ -29,8 +27,8 @@ del /f/s/q %PUBLISH_PATH%\TEMP > nul & rmdir /s/q %PUBLISH_PATH%\TEMP
 :: portable self-contained
 echo publishing portable self-contained version...
 dotnet publish --nologo -r win-x64 -c Release --self-contained true -p:PublishSingleFile=false -o %PUBLISH_PATH%\TEMP
-ren %PUBLISH_PATH%\TEMP\DnsTube.exe DnsTube-Portable-SelfContained%EXE_VERSION%.exe
-ren %PUBLISH_PATH%\TEMP\DnsTube.pdb DnsTube-Portable-SelfContained%EXE_VERSION%.pdb
+ren %PUBLISH_PATH%\TEMP\DnsTube.exe DnsTube-Portable-SelfContained.exe
+ren %PUBLISH_PATH%\TEMP\DnsTube.pdb DnsTube-Portable-SelfContained.pdb
 
 cd %PUBLISH_PATH%\TEMP
 %ZIP_EXE_PATH% a -t7z -r -mx9 ..\DnsTube-Portable-SelfContained%EXE_VERSION%.7z *.*
@@ -44,8 +42,8 @@ set PORTABLE=
 ::regular
 echo publishing regular version...
 dotnet publish --nologo -r win-x64 -c Release --self-contained false -o %PUBLISH_PATH%\TEMP
-ren %PUBLISH_PATH%\TEMP\DnsTube.exe DnsTube%EXE_VERSION%.exe
-ren %PUBLISH_PATH%\TEMP\DnsTube.pdb DnsTube%EXE_VERSION%.pdb
+ren %PUBLISH_PATH%\TEMP\DnsTube.exe DnsTube.exe
+ren %PUBLISH_PATH%\TEMP\DnsTube.pdb DnsTube.pdb
 
 cd %PUBLISH_PATH%\TEMP
 %ZIP_EXE_PATH% a -t7z -r -mx9 ..\DnsTube%EXE_VERSION%.7z *.*
@@ -55,8 +53,8 @@ del /f/s/q %PUBLISH_PATH%\TEMP > nul & rmdir /s/q %PUBLISH_PATH%\TEMP
 :: regular self-contained
 echo publishing regular self-contained version...
 dotnet publish --nologo -r win-x64 -c Release --self-contained true -p:PublishSingleFile=false -o %PUBLISH_PATH%\TEMP
-ren %PUBLISH_PATH%\TEMP\DnsTube.exe DnsTube-SelfContained%EXE_VERSION%.exe
-ren %PUBLISH_PATH%\TEMP\DnsTube.pdb DnsTube-SelfContained%EXE_VERSION%.pdb
+ren %PUBLISH_PATH%\TEMP\DnsTube.exe DnsTube-SelfContained.exe
+ren %PUBLISH_PATH%\TEMP\DnsTube.pdb DnsTube-SelfContained.pdb
 
 cd %PUBLISH_PATH%\TEMP
 %ZIP_EXE_PATH% a -t7z -r -mx9 ..\DnsTube-SelfContained%EXE_VERSION%.7z *.*
