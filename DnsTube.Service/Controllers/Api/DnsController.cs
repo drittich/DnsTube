f3d2b838d1ac21f4a3490bf7bcc013ce192267af
D'Arcy Rittich <drittich@gmail.com>
2024-08-26 08:42:56 -0400

    cleanup

--- DnsTube.Service/Controllers/Api/DnsController.cs
+++ DnsTube.Service/Controllers/Api/DnsController.cs
@@ -2,8 +2,6 @@
 
 using Microsoft.AspNetCore.Mvc;
 
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
 namespace DnsTube.Service.Controllers.Api
 {
 	[Route("api/[controller]")]
